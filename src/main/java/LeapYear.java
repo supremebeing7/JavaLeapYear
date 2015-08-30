fd891e9964d177bd26ff78f947abdd4cc788178e
Mark Lehman <markopolo@gmail.com>
2015-08-29 22:56:15 -0700

    turn leap-year into templated gradle project

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,6 +1,33 @@
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
 public class LeapYear {
-  public static void main(String[] args) {}
-  public Boolean isLeapYear(Integer year) {
+  public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/detector", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/detector.vtl");
+
+      String year = request.queryParams("year");
+      Integer yearNumber = Integer.parseInt(year);
+      Boolean isLeapYear = isLeapYear(yearNumber);
+      model.put("isLeapYear", isLeapYear);
+      model.put("year", year);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
+
+  public static Boolean isLeapYear(Integer year) {
     if ( year % 400 == 0 ) {
       return true;
     } else {
